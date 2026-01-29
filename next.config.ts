94d3e444a0ebbf47593ebee55121da53610a6b8c
thomas regourd <thomasregourd@Host-002.lan>
2026-01-29 19:29:05 +0100

    add next.config.ts for headers

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,42 @@
 import type { NextConfig } from "next";
 
+const isDev = process.env.NODE_ENV === "development";
+
+const cspHeader = `
+  default-src 'self';
+  base-uri 'self';
+  form-action 'self';
+  frame-ancestors 'self';
+  object-src 'none';
+  upgrade-insecure-requests;
+  script-src 'self' 'unsafe-inline' ${isDev ? "'unsafe-eval'" : ""} https://challenges.cloudflare.com;
+  style-src 'self' 'unsafe-inline';
+  img-src 'self' blob: data:;
+  font-src 'self';
+  frame-src https://challenges.cloudflare.com;
+  connect-src 'self' https://challenges.cloudflare.com;
+`
+  .replace(/\s{2,}/g, " ")
+  .trim();
+
 const nextConfig: NextConfig = {
-  /* config options here */
+  poweredByHeader: false,
+  async headers() {
+    return [
+      {
+        source: "/:path*",
+        headers: [
+          { key: "X-DNS-Prefetch-Control", value: "on" },
+          { key: "Strict-Transport-Security", value: "max-age=63072000; includeSubDomains; preload" },
+          { key: "X-Frame-Options", value: "SAMEORIGIN" },
+          { key: "X-Content-Type-Options", value: "nosniff" },
+          { key: "Referrer-Policy", value: "origin-when-cross-origin" },
+          { key: "Permissions-Policy", value: "camera=(), microphone=(), geolocation=(), payment=(), usb=()" },
+          { key: "Content-Security-Policy", value: cspHeader },
+        ],
+      },
+    ];
+  },
 };
 
 export default nextConfig;
